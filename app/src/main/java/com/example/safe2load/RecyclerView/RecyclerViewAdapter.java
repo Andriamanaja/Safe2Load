ac7f29eee528b43dc3cf87979f21d7f16ca55372
fenoh <fenoh.andrianaly@gmail.com>
2020-05-08 16:08:51 +0200

    item

--- app/src/main/java/com/example/safe2load/RecyclerView/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/safe2load/RecyclerView/RecyclerViewAdapter.java
@@ -4,17 +4,11 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.SupportActivity;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.transition.AutoTransition;
-import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -114,5 +108,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             this.item_stat = itemView.findViewById(R.id.item_stat) ;
         }
     }
+
+
 
 }
