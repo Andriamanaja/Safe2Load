11709193bb9361363f13ef6772c1efd0002eaaf3
fenoh <fenoh.andrianaly@gmail.com>
2020-06-02 17:34:06 +0200

    design

--- app/src/main/java/com/example/safe2load/HomeFragment.java
+++ app/src/main/java/com/example/safe2load/HomeFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,14 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    CardView btn_acc_operation ;
+    CardView btn_acc_stat ;
+    CardView btn_acc_sync ;
+    CardView btn_acc_params ;
+    CardView btn_acc_dsc;
+
+    View view;
 
     private OnFragmentInteractionListener mListener;
 
@@ -37,17 +47,77 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false) ;
+        this.initView();
+        //content_operation = view.findViewById(R.id.content_operation) ;
+        return view;
+    }
+
+    private void initView() {
+        btn_acc_operation = view.findViewById(R.id.acc_operation);
+        btn_acc_params = view.findViewById(R.id.acc_params);
+        btn_acc_stat = view.findViewById(R.id.acc_stat);
+        btn_acc_sync = view.findViewById(R.id.acc_sync);
+        btn_acc_dsc = view.findViewById(R.id.acc_dsc);
+
+        btn_acc_operation.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            try {
+                fragmentManager.beginTransaction().replace(R.id.layout_content, OperationFragment.class.newInstance()).commit();
+                getActivity().setTitle("Opération");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (java.lang.InstantiationException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btn_acc_params.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            try {
+                fragmentManager.beginTransaction().replace(R.id.layout_content,ParamsFragment.class.newInstance()).commit();
+                getActivity().setTitle("Paramètres");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (java.lang.InstantiationException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btn_acc_sync.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            try {
+                fragmentManager.beginTransaction().replace(R.id.layout_content, SynchroFragment.class.newInstance()).commit();
+                getActivity().setTitle("Synchronisation");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (java.lang.InstantiationException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btn_acc_stat.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            try {
+                fragmentManager.beginTransaction().replace(R.id.layout_content, StatFragment.class.newInstance()).commit();
+                getActivity().setTitle("Statistique des opérations");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (java.lang.InstantiationException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btn_acc_dsc.setOnClickListener(v -> {
+
+
+        });
     }
 
     public void onButtonPressed(Uri uri) {
